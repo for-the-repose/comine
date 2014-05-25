ea435055586d4973e0739cb933f6204c901963bf
Alexander Soloviëv <no.friday@yandex.ru>
2014-05-25 22:22:40 +0400

    Core dump files layout provider iface definition

---
+++ source/comine/iface/infer.py
@@ -0,0 +1,28 @@
+#__ LGPL 3.0, 2014 Alexander Soloviev (no.friday@yandex.ru)
+
+class ILayout(object):
+    def __binary__(s):
+        ''' Path to binary executable '''
+
+        raise Exception('not implemented')
+
+    def __core__(s):
+        ''' Path to core image file '''
+
+        raise Exception('not implemented')
+
+    def __maps__(s):
+        '''
+            Optional copy of /proc/${pid}/maps file made just before
+            core generation, or while this process.
+        '''
+
+        return None
+
+    def __root__(s):
+        '''
+            Optional path to prepared root directory with actual
+            shared objects linked with binaty and its debug symbols.
+        '''
+
+        return None
